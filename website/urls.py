554986b27a4387e383e676690f5b5f3853440801
dkosarevsky <kosarevsky.d@ya.ru>
2020-07-06 00:51:40 +0300

    add contact page

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact.html', views.contact, name="contact"),
 ]
 
 # {% static 'website/css/style.css' %}
--- website/views.py
+++ website/views.py
@@ -3,3 +3,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+
+def contact(request):
+    if request.method == 'POST':
+        message_name = request.POST['message-name']
+        message_email = request.POST['message-email']
+        message = request.POST['message']
+        return render(request, 'contact.html', {'message_name': message_name})
+    else:
+        return render(request, 'contact.html', {})
